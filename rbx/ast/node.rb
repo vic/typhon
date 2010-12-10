3275dda3d1dcf656aafad3e766dd2dbc66a389f4
Victor Hugo Borja <vborja@apache.org>
2010-12-09 18:40:37 -0600

    Typhon::AST.build used to construct the AST tree from sexp.

--- rbx/ast/node.rb
+++ rbx/ast/node.rb
@@ -1,8 +1,25 @@
 class Typhon
   class AST
 
-    def self.build(ary)
+    Nodes = Hash.new
 
+    # Converts a ruby sexp (array literals) into Typhon AST
+    def self.build(sexp)
+      return nil if sexp.nil? || sexp.empty?
+      name = sexp.first
+      rest = sexp[1..-1]
+      type = Nodes[name]
+      raise "Unknown sexp type: #{name}" unless type
+      body = rest.map { |e| if e.kind_of?(Array) then from_sexp(e) else e end }
+      line = 0
+      ast = type.new(line, *body)
+      ast
+    end
+
+    class Node < Rubinius::AST::Node
+      def self.name(name)
+        Nodes[name] = self
+      end
     end
 
   end
