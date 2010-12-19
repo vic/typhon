279cc889d66428e0939e31bd5622f21b85f53e80
Graham Batty <graham-storm-git@ript.net>
2010-12-18 20:36:48 -0700

    Made print work a little more like it's supposed to.
    Outputs args separated by spaces and then a \n at the end.

--- lib/typhon/environment/built_in.rb
+++ lib/typhon/environment/built_in.rb
@@ -2,7 +2,7 @@ module Typhon
   module Environment
     module BuiltIn
       def __print_to__(out, *args)
-        out.puts(args)
+        out.print(args.join(' ') + "\n")
       end
       def __print__(*args)
         __print_to__($stdout,*args)
